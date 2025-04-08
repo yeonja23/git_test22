73ae165b29dcfc811b9b2d7865afbc7664cf2366
Park, Yeonji <yeonpyj23@gmail.com>
2025-04-08 21:42:34 +0900

    Main.java 수정

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Hello h = new Hello();
-        h.say();
+        Hello hello = new Hello();
+        hello.say();
     }
 }
