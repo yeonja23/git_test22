1a85b8a0a8dffe9401351e2b119c6e7c25a9beb9
Park, Yeonji <yeonpyj23@gmail.com>
2025-04-08 21:48:07 +0900

    Hello 클래스 수정

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,15 @@
 public class Hello {
-    String message = "안녕하세요";
-    public void say() { System.out.println(message); }
+    String message;
+
+    public Hello() {
+        this("안녕하세요");
+    }
+
+    public Hello(String message) {
+        this.message = message;
+    }
+
+    public void say() {
+        System.out.println(message);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Hello h = new Hello();
+        Hello h = new Hello("hello world");
         h.say();
     }
 }
