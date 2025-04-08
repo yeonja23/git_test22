8842cf0d4fab536cb31f7f87877d95d5d602f4fe
Park, Yeonji <yeonpyj23@gmail.com>
2025-04-08 23:35:30 +0900

    화면 출력 방법 수정

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,9 @@
 public class Hello {
+
     String message = "안녕하세요";
-    public void say() { System.out.println(message); }
+
+    public String say() {
+        return message;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
+
     public static void main(String[] args) {
         Hello hello = new Hello();
-        hello.say();
+        System.out.println(hello.say());
     }
+    
 }
